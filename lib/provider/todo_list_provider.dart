b5023167b4478923784af8a1196d71db26993704
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-01-27 17:05:48 -0500

    Implement shared preferences for todo list persistence and update project configuration files

--- lib/provider/todo_list_provider.dart
+++ lib/provider/todo_list_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_provider/model/todo.dart';
 
 class TodoListProvider extends ChangeNotifier {
@@ -6,21 +9,49 @@ class TodoListProvider extends ChangeNotifier {
 
   List<Todo> get todos => _todoList;
 
+  TodoListProvider() {
+    _loadTodos();
+  }
+
   // addTodo
   void addTodo(String title) {
     _todoList.add(Todo(title: title));
+    _saveTodos();
     notifyListeners();
   }
 
   // removeTodo
   void removeTodo(int index) {
     _todoList.removeAt(index);
+    _saveTodos();
     notifyListeners();
   }
 
   // toggleTodo
   void toggleTodo(int index) {
     _todoList[index].toggleDone();
+    _saveTodos();
     notifyListeners();
+  }
+
+  // loadTodos from sharedPreferences
+  Future<void> _loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final todosString = prefs.getString('todos');
+    if (todosString != null) {
+      final _todosJson = jsonDecode(todosString) as List;
+      _todoList.addAll(_todosJson
+          .map((todo) => Todo(title: todo['title'], isDone: todo['isDone'])));
+      notifyListeners();
+    }
+  }
+
+  // saveTodos in sharedPreferences
+  Future<void> _saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final todosJson = _todoList
+        .map((todo) => {'title': todo.title, 'isDone': todo.isDone})
+        .toList();
+    await prefs.setString('todos', jsonEncode(todosJson));
   }
 }
